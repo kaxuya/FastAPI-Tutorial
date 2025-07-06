44f3025cec09f5e1b310c2d5065a6751bfc9d176
Kaxuya <218813421+kaxuya@users.noreply.github.com>
2025-07-06 20:58:11 +0530

    feat: add /view endpoint

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json') as f:
+        data = json.load(f)
+    
+    return data
+
 @app.get("/")
 def hello():
-    return {"hello": "world"}
+    return {"message": "Patient Management System API"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
